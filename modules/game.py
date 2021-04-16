19840cba73398878fd149ff6f5788617bcbcfabe
Spencer <Spencer.Taber@yahoo.com>
2021-04-16 16:18:46 +0100

    unit testing for scissors complete, beginning unit testing for paper

--- modules/game.py
+++ modules/game.py
@@ -10,6 +10,11 @@ class Game:
             if player_2.choice == "scissors" or player_2.choice == "bazooka":
                 return player_1
         
+        if player_1.choice == "scissors":
+            if player_2.choice == "paper" or player_2.choice == "bazooka":
+                return player_1
+    
+        
         
         return player_2
     
--- test_modules/test_game.py
+++ test_modules/test_game.py
@@ -6,6 +6,7 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.rock = Player("John", "rock")
         self.scissors = Player("Sophie", "scissors")
+        self.paper = Player("Peter", "paper")
         self.bazooka = Player("Barry", "bazooka")
     
 
@@ -29,8 +30,21 @@ class TestGame(unittest.TestCase):
         result = Game.get_winning_choice(self.bazooka, self.rock)
         self.assertEqual(self.rock, result)
     
-    # def test_scissors_beats_paper(self):
-
+    def test_scissors_beats_paper(self):
+        result = Game.get_winning_choice(self.scissors, self.paper)
+        self.assertEqual(self.scissors, result)
+    
+    def test_paper_loses_to_scissors(self):
+        result = Game.get_winning_choice(self.paper, self.scissors)
+        self.assertEqual(self.scissors, result)
+    
+    def test_scissors_beats_bazooka(self):
+        result = Game.get_winning_choice(self.scissors, self.bazooka)
+        self.assertEqual(self.scissors, result)
+    
+    def test_bazooka_loses_to_scissors(self):
+        result = Game.get_winning_choice(self.bazooka, self.scissors)
+        self.assertEqual(self.scissors, result)
     
 
 
