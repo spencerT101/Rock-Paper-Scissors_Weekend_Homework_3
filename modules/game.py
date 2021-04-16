792da20faac980fb086cd145995c4cc66ac8cb5c
Spencer <Spencer.Taber@yahoo.com>
2021-04-16 15:50:44 +0100

    Same choice and rock beats scissors unit testing complete

--- modules/game.py
+++ modules/game.py
@@ -1,6 +1,16 @@
-class Game():
+class Game:
 
-    def __init__(self,rock, paper, scissors):
-        self.rock = rock
-        self.paper = paper
-        self.done = scissors
+
+    def get_winning_choice(player_1, player_2):
+
+        if player_1.choice == player_2.choice:
+            return None
+        
+        if player_1.choice == "rock" and player_2.choice == "scissors":
+            return player_1
+        
+        
+        return player_2
+    
+
+     
--- test_modules/test_game.py
+++ test_modules/test_game.py
@@ -1,11 +1,29 @@
 import unittest
-from modules.game import Game
+from modules.game import *
+from modules.player import Player
 
 class TestGame(unittest.TestCase):
     def setUp(self):
-        self.john = Player("John", "rock")
-        self.sophie = Player("Sophie", "scissors")
-        
+        self.rock = Player("John", "rock")
+        self.scissors = Player("Sophie", "scissors")
+    
+
+    def test_same_player_choice_equals_None(self):
+        result = Game.get_winning_choice(self.rock,self.rock)
+        self.assertEqual(None, result)
+
+    def test_rock_beats_scissors(self):
+        result = Game.get_winning_choice(self.rock, self.scissors)
+        self.assertEqual(self.rock, result)
+    
+    def test_scissors_loses_to_rock(self):
+        result = Game.get_winning_choice(self.scissors, self.rock)
+        self.assertEqual(self.rock, result)
+    
+    def 
+
+    
+
 
     
 
