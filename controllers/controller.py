6883fd00d5b61f925cba3e3526a7464da2cdb2cc
Spencer <Spencer.Taber@yahoo.com>
2021-04-17 14:20:27 +0100

    refactored code for flask environment, inital tests in browser working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,15 +1,13 @@
 from flask import render_template, request
 from app import app
 from modules.player import Player 
-from modules.game import Game
+from modules.game import get_winning_choice
 
 @app.route('/games/<player_1>/<player_2>')
-def result(player_1, player_2):
 
-    if result == "get_winning_choice":
-        return f"The winner is {game.get_winning_choice}"
+def result(player_1, player_2):
+    return f"The winner is {get_winning_choice(player_1, player_2)}"
     
-    else:
-        return "Player 2 wins"
+    
 
 
--- modules/game.py
+++ modules/game.py
@@ -1,25 +1,20 @@
-class Game:
+from modules.player import Player
 
-    def get_winning_choice(player_1, player_2):
+def get_winning_choice(player_1, player_2):
+    if player_1 == player_2:
+        return None
+        
+    if player_1 == "rock":
+        if player_2 == "scissors" or player_2 == "bazooka":
+            return player_1
+        
+    if player_1 == "scissors":
+        if player_2 == "paper" or player_2 == "bazooka":
+            return player_1
+        
+    if player_1 == "paper":
+        if player_2 == "rock" or player_2 == "bazooka":
+            return player_1
+        
+    return player_2
 
-        if player_1.choice == player_2.choice:
-            return None
-        
-        if player_1.choice == "rock":
-            if player_2.choice == "scissors" or player_2.choice == "bazooka":
-                return player_1
-        
-        if player_1.choice == "scissors":
-            if player_2.choice == "paper" or player_2.choice == "bazooka":
-                return player_1
-        
-        if player_1.choice == "paper":
-            if player_2.choice == "rock" or player_2.choice == "bazooka":
-                return player_1
-    
-        
-        
-        return player_2
-    
-
-     
